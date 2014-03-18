416acadfd524dd82f38fe3b161a1537ccf96a667
ofir agahi <iofirag@gmail.com>
2014-03-18 14:28:03 +0200

    fix date

--- time.js
+++ time.js
@@ -3,8 +3,8 @@ $('body').prepend('<h1 style="text-align:center" class="get-time"></h1>');
 $('.get-time').ready(function(){
 var date = new Date();
 var year    = date.getFullYear();
-var month   = date.getMonth();
-var day     = date.getDay();
+var month   = date.getMonth()+1;
+var day     = date.getDate();
 var hour    = date.getHours();
 var minute  = date.getMinutes();
 var seconds = date.getSeconds();
